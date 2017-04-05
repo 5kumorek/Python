d18cb4339238b257d3be9e278797d85c44af6db9
5kumorek <radoslaw.kumorekit@gmail.com>
2017-04-05 15:03:07 +0200

    Dodałem testy

---
+++ Lab3/BoardTests.py
@@ -0,0 +1,43 @@
+import unittest
+import Board
+
+class BoardTests(unittest.TestCase):
+    def test_check_players_move_is_correct(self):
+        MyGame = Board.Board(4, 'x')
+        x,y=2,2
+        MyGame.movePlayer(x, y)
+        self.assertEqual(self.arrayPlayer[x][y], 1)
+        self.assertEqual(self.arrayGame[x][y], 1)
+
+    def test_wchich_checks_Players_win(self):
+        MyGame = Board.Board(4, 'x')
+        MyGame.movePlayer(0,0)
+        MyGame.movePlayer(1,1)
+        MyGame.movePlayer(2,2)
+        MyGame.movePlayer(3,3)
+        self.assertEqual(MyGame.checkPlayersWin(),true)
+
+    def test_wchich_checks_CPU_win(self):
+        MyGame = Board.Board(4, 'x')
+        MyGame.movePlayer(0, 0)
+        MyGame.movePlayer(1, 1)
+        MyGame.movePlayer(2, 2)
+        MyGame.movePlayer(3, 3)
+        self.assertEqual(MyGame.checkCPUsWin(), true)
+
+
+    def test_check_if_diagonal_is_full(self):
+        MyGame = Board.Board(4, 'x')
+        MyGame.movePlayer(0, 0)
+        MyGame.movePlayer(1, 1)
+        MyGame.movePlayer(2, 2)
+        MyGame.movePlayer(3, 3)
+        self.assertEqual(MyGame.checkDiagonal(), true)
+
+    def test_check_if_index_is_out_of_array(self):
+        MyGame = Board.Board(4, 'x')
+        self.assertRaises(OutOfBoard, MyGame.movePlaye, 6, 6)
+
+
+if __name__=='__main__':
+    unittest.main(exit=False)
