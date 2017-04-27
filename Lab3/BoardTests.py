ff4f214124d2778ba7f80585e181566683820dc8
5kumorek <radoslaw.kumorekit@gmail.com>
2017-04-28 00:38:52 +0200

    Kolejnny raz poprawiłem testy

--- Lab3/BoardTests.py
+++ Lab3/BoardTests.py
@@ -1,28 +1,29 @@
 import unittest
 import Board
+import Validator
 
 class BoardTests(unittest.TestCase):
     def test_check_players_move_is_correct(self):
         MyGame = Board.Board(4, 'x')
         x,y=2,2
         MyGame.movePlayer(x, y)
-        self.assertEqual(self.arrayPlayer[x][y], 1)
-        self.assertEqual(self.arrayGame[x][y], 1)
+        self.assertEqual(MyGame.arrayPlayer[x][y], 1)
+        self.assertEqual(MyGame.arrayGame[x][y], 1)
 
     def test_wchich_checks_Players_win(self):
-        MyGame = Board.Board(4, 'x')
-        MyGame.movePlayer(0,0)
-        MyGame.movePlayer(1,1)
-        MyGame.movePlayer(2,2)
-        MyGame.movePlayer(3,3)
-        self.assertEqual(MyGame.checkPlayersWin(),True)
-
-    def test_wchich_checks_CPU_win(self):
         MyGame = Board.Board(4, 'x')
         MyGame.movePlayer(0, 0)
         MyGame.movePlayer(1, 1)
         MyGame.movePlayer(2, 2)
         MyGame.movePlayer(3, 3)
+        self.assertEqual(MyGame.checkPlayersWin(),True)
+
+    def test_wchich_checks_CPU_win(self):
+        MyGame = Board.Board(4, 'x')
+        MyGame.arrayCPU[0][0] = 1
+        MyGame.arrayCPU[1][1] = 1
+        MyGame.arrayCPU[2][2] = 1
+        MyGame.arrayCPU[3][3] = 1
         self.assertEqual(MyGame.checkCPUsWin(), True)
 
 
@@ -36,7 +37,7 @@ class BoardTests(unittest.TestCase):
 
     def test_check_if_index_is_out_of_array(self):
         MyGame = Board.Board(4, 'x')
-        self.assertRaises(OutOfBoard, MyGame.movePlaye, 6, 6)
+        self.assertRaises(IndexError, MyGame.movePlayer, 6, 6)
 
 
 if __name__=='__main__':
